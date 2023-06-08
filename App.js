a07142f0cbbfdf4d5a3c2681e003012d5ed4b0cb
VarunHarish <varunharish98@gmail.com>
2023-06-08 16:29:21 +0530

    Coding Assignment - Using createElement

--- App.js
+++ App.js
@@ -1,69 +1,38 @@
-// const heading = React.createElement("h1", {
-//     id: "heading",
-//     abc: "xyz"
-// }, "Hello World from React!!!"); //Step 1 - Create an element
-// const root = ReactDOM.createRoot(document.getElementById("root")); //Step 2 - Create a root in which element needs to be rendered
-// root.render(heading); //Step 3 - Render the elements in the root
-
-//Creating for nested elements for structure
-/**
- * <div>
- *      <div>
- *             <h1> This is h1 heading </h1>
- *              <h1> This is h1 heading </h1>
- *      </div>
- *       <div>
- *             <h1> This is h1 heading </h1>
- *              <h1> This is h1 heading </h1>
- *      </div>
- * </div>
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+//Assignment-1
 const headings = React.createElement(
   "div",
   {
-    id: "parent",
+    className: "title",
   },
-  [React.createElement(
-    "div",
-    {
-      id: "child",
-    },
-    [React.createElement(
+
+  [
+    React.createElement(
       "h1",
       {
         id: "heading",
       },
       "This is h1 heading"
-    ), 
+    ),
     React.createElement(
-        "h2",
-        {
-          id: "heading",
-        },
-        "This is h2 heading"
-      )]
-  ), React.createElement(
-    "div",
-    {
-      id: "child",
-    },
-    [React.createElement(
-      "h1",
+      "h2",
       {
         id: "heading",
       },
-      "This is h1 heading"
-    ), 
+      "This is h2 heading"
+    ),
     React.createElement(
-        "h2",
-        {
-          id: "heading",
-        },
-        "This is h2 heading"
-      )]
-  )]
-); //Nested elements 
+      "h3",
+      {
+        id: "heading",
+      },
+      "This is h3 heading"
+    ),
+  ]
+); //Nested elements
 const root = ReactDOM.createRoot(document.getElementById("root")); //Create root
-root.render(headings); 
+root.render(headings);
+
+//Assignment - 2; Using JSX
